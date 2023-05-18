f7397bf51d4bec943ea7ac9c347f28f71782399c
Odeyale Kehinde <odeyale_kehinde@yahoo.com>
2023-05-18 11:22:17 +0100

    Addition of EventService

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,8 +1,24 @@
 <template>
-  <h1>Showing Event #{{ id }}</h1>
+  <h1>Showing Event: {{ event.title }}</h1>
 </template>
 <script>
+import EventService from "@/services/EventService";
+
 export default {
   props: ["id"],
+  data() {
+    return {
+      event: {},
+    };
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data;
+      })
+      .catch((error) => {
+        console.log("There was an error", error.response);
+      });
+  },
 };
 </script>
