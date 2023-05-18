1c4b9889df05cdd60a744918fbaaa3cc918e10ef
Odeyale Kehinde <odeyale_kehinde@yahoo.com>
2023-05-18 11:36:11 +0100

    EventShow update

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,5 +1,35 @@
 <template>
-  <h1>Showing Event: {{ event.title }}</h1>
+  <div>
+    <header class="event-header">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h1 class="title">{{ event.title }}</h1>
+      <h5>Organized by {{ event.organizer ? event.organizer.name : "" }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </header>
+
+    <BaseIcon name="map"><h2>Location</h2></BaseIcon>
+    <address>{{ event.location }}</address>
+
+    <h2>Event Details</h2>
+    <p>{{ event.description }}</p>
+
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">{{
+        event.attendees ? event.attendees.length : 0
+      }}</span>
+    </h2>
+
+    <ul class="list-group">
+      <li
+        v-for="(attendee, index) in event.attendees"
+        :key="index"
+        class="list-item"
+      >
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 import EventService from "@/services/EventService";
@@ -22,3 +52,27 @@ export default {
   },
 };
 </script>
+<style>
+.location {
+  margin-bottom: 0;
+}
+
+.location > .icon {
+  margin-left: 10px;
+}
+
+.event-header > .title {
+  margin: 0;
+}
+
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px e5e5e5;
+}
+</style>
